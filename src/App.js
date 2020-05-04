777dca63c93d72babd81929a641c3e6291f91378
Sofia Levin <sofiafontes@me.com>
2020-05-04 15:21:38 -0700

    Added social icons.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Coming soon...</h1>
+        <h1 className="content">Coming soon...</h1>
       </header>
+      <footer className="links">
+        <a href="https://github.com/sofialevin">
+          <FontAwesomeIcon icon={faGithub} />
+        </a>
+        <a href="https://www.linkedin.com/in/sofiaflevin/">
+          <FontAwesomeIcon icon={faLinkedin} />
+        </a>
+        <a href="https://twitter.com/sofiaflevin">
+          <FontAwesomeIcon icon={faTwitter} />
+        </a>
+      </footer>
     </div>
   );
 }
