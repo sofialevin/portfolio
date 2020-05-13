d6d949542393a1b0b68938dce7d45b40084e05d9
Sofia Levin <sofiafontes@me.com>
2020-05-13 15:39:57 -0700

    Updated tech stack for projects.

--- src/Projects.js
+++ src/Projects.js
@@ -10,8 +10,11 @@ const Projects = () => {
   const projects = [
     {
       name: "Alpaca Vids",
+      in_progress: false,
+      internal: false,
       description: ["Collaborated in a team of 6 over the course of 2 months to plan and build a React and Node.js video recording app using the MediaDevices web API.", "The application allows students to record themselves answering interview questions in order to get practice before they start interviewing for jobs."],
-      tech: ["react", "redux", "nodejs"],
+      tech: ["react", "redux", "nodejs", "express", 
+      "postgresql", "awsS3"],
       img: AlpacaGif,
       github: "https://github.com/Lambda-School-Labs/video-journal-for-teams-fe",
       site: "https://www.alpacavids.com"
@@ -19,8 +22,11 @@ const Projects = () => {
     },
     {
       name: "KnitHub",
+      in_progress: true,
+      internal: false,
       description: ["Web app to find knitting tutorials for various techniques, with options to filter by source, type of tutorial (video or picture), type of technique, and more."],
-      tech: ["react", "redux"],
+      tech: ["react", "redux", "nodejs", "express", 
+      "postgresql"],
       img: "https://p198.p4.n0.cdn.getcloudapp.com/items/Z4uLp5l4/Screen%20Recording%202020-05-12%20at%2008.01%20PM.gif?v=9df0f357e6a721b4fa1e252c3eda02de"
     },
   ]
