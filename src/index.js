74c43ba1d3dd7967891df91dca354c06303e1313
Sofia Levin <sofiafontes@me.com>
2020-06-25 23:58:37 -0700

    Added google analytics.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import ReactGA from 'react-ga';
 import App from './App';
+
+Analytics tracking ID
+ReactGA.initialize(trackingId);
+ReactGA.pageview(window.location.pathname + window.location.search);
+// ReactGA.set({
+//   userId: auth.currentUserId(),
+//   // any data that is relevant to the user session
+//   // that you would like to track with google analytics
+// });
 
 ReactDOM.render(
   <React.StrictMode>
